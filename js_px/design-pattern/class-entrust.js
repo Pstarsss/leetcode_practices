3b0ba394a5dbd0be81d7758a588250baa659fb97
panxing <xingpan@yangqianguan.com>
2021-10-09 18:29:49 +0800

    note

---
+++ js_px/design-pattern/class-entrust.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: xingpan 
+ * @Date: 2021-10-09 16:09:20 
+ * @Last Modified by: xingpan
+ * @Last Modified time: 2021-10-09 16:24:47
+ */
+
+function Foo(who) {
+    this.me = who;
+}
+
+Foo.prototype.identify = function() {
+    return `I am ${this.me}`;
+}
+
+function Bar(who) {
+    Foo.call(this, who);
+}
+
+Bar.prototype = Object.create(Foo.prototype);
+
+Bar.prototype.speak = function() {
+    console.log(`Hello, ${this.identify()}.`);
+}
+
+let b1 = new Bar('b1');
+let b2 = new Bar('b2');
+console.log(Bar);
+console.log(Bar.prototype);
+
+
+b1.speak();
+b2.speak();
+
